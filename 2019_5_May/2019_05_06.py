bbfcd01e060974391c0171467945142451cf1f0c
alstn2468 <alstn2468_@naver.com>
2019-05-06 14:13:38 +0900

    Add 2019_05_06.py

---
+++ 2019_5_May/2019_05_06.py
@@ -0,0 +1,6 @@
+
+# 양수 K가 주어지면 K 길이의 이진법 숫자를 모두 프린트하시오. 단, 연속으로 1이 있으면 안됩니다.
+# Given an integer K, print all binary strings of length K without consecutive 1s.
+#
+# input:5
+# output: 00000 00001 00010 00100 00101 01000 01001 01010 10000 10001 10010 10100 10101

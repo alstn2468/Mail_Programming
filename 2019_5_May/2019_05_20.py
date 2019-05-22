3453580e0557dd63cf0cb0ca9f5ccf1a4483fc96
alstn2468 <alstn2468_@naver.com>
2019-05-22 14:13:38 +0900

    Update 2019 05 20 problem

---
+++ 2019_5_May/2019_05_20.py
@@ -0,0 +1,6 @@
+
+# 0, 1, 2로 이루어진 배열을 가장 효율적으로 정렬 하시오. 시간복잡도 O(n).
+# Given an array consisting of 0, 1 and 2s, sort this array.
+#
+# Input: [0, 1, 2, 2, 0, 0, 0, 1] 
+# Output: [0, 0, 0, 0, 1, 1, 2, 2]

3c1a2ebad6c12f3f6d6b6fc5a7d71fc7e658e097
alstn2468 <alstn2468_@naver.com>
2019-06-18 13:09:49 +0900

    Update 2019 06 17 problem

---
+++ 2019_6_Jun/2019_06_17.py
@@ -0,0 +1,6 @@
+
+# L0→L1→…→Ln-1→Ln 의 단일 연결 리스트가 주어지면 순서를 다음과 같이 바꾸시오: L0→Ln→L1→Ln-1→L2→Ln-2→…
+# Given a singly linked list L: L0→L1→…→Ln-1→Ln, reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+# 
+# input: 1 -> 2 -> 3 -> 4
+# output: 1 -> 4 -> 2 -> 3

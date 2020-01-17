35b70f2a86f69256bacb0b33f834aa10eef06f29
Kimminsu <alstn2468_@naver.com>
2020-01-17 09:00:23 +0900

    Add 2020_01_13 problem

---
+++ 2020_1_Jan/2020_01_13.py
@@ -0,0 +1,8 @@
+
+# 사전 순서 상 가장 앞의 문자열 회전 (Lexicographically minimal string rotation)은
+# 주어진 문자열을 돌려서 (맨 앞의 문자가 맨 뒤로 가거나, 맨 뒤의 문자가 맨 뒤로 가도록 변경)
+# 만들 수 있는 문자열 중 사전 순서 상 가장 앞에 오는 문자열을 찾는 문제입니다.
+# 주어진 문자열을 회전시켜 사전 순서 상 가장 앞의 문자열을 찾으시오.
+#
+# Input: bbaaccaadd
+# Output: aaccaaddbb

af71fed6fd2dc99ea725b8bb14d7eea8449b08be
Kimminsu <alstn2468_@naver.com>
2020-06-15 09:36:11 +0900

    Create 2020_06_15.py

---
+++ 2020_6_Jun/2020_06_15.py
@@ -0,0 +1,17 @@
+
+# 양의 정수가 주어 졌을 때, 주어진 수에 해당하는 엑셀 컬럼 이름을 구하시오.
+#
+# Input: 1
+# Output: A
+#
+# Input: 26
+# Output: Z
+#
+# Input: 27
+# Output: AA
+#
+# Input: 106
+# Output: DB
+#
+# Input: 706
+# Output: AAD

a86b0ab9f4dc139b83d0c0cb7fbdcd9af2b3ffd2
Kimminsu <alstn2468_@naver.com>
2020-06-29 09:04:05 +0900

    Create 2020_06_29.py

---
+++ 2020_6_Jun/2020_06_29.py
@@ -0,0 +1,20 @@
+
+# 단어 X와 Y가 있을때, X의 문자들을 이용해 Y를 만들 수 있다면 두 단어는 아나그램이라고 합니다.
+# 예를 들어 다음 단어들은 아나그램입니다.
+# actors - costar
+# altered - related
+# auctioned - education
+# aspired - despire
+# mastering- streaming
+# recured - secured
+# 단어 리스트가 주어졌을 때, 서로 아나그램인 단어를 묶어서 출력 하시오.
+#
+# Input: [CARS, REPAID, DUES, NOSE, SIGNED, LANE, PAIRED, ARCS, GRAB, USED, ONES, BRAG, SUED, LEAN, SCAR, DESIGN]
+# Output:
+# GRAB BRAG
+# LANE LEAN
+# SIGNED DESIGN
+# CARS ARCS SCAR
+# REPAID PAIRED
+# DUES USED SUED
+# NOSE ONES
